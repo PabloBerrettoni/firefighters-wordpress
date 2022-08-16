4e1b627974b043f321566df6551facf1d498e1e8
PabloBerrettoni <pablo_berrettoni@hotmail.com>
2022-08-16 16:29:20 -0300

    changing images position in profile view

--- themes/firefightersTheme/page-userProfile.php
+++ themes/firefightersTheme/page-userProfile.php
@@ -13,10 +13,10 @@ include ('templates/partials/header.php');
 
 
 <div class="profileMain">
-    <img src="<? echo get_template_directory_uri(); ?><?php echo $logo ?>" alt="profile image">
+    <img src="<? echo get_template_directory_uri(); ?><?php echo $maleProfile ?>" alt="headquarters department">
     <h2>Name of user</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque doloribus, aperiam, adipisci veritatis, provident rem libero quaerat enim accusamus fuga nihil reprehenderit voluptatum vitae repellat perspiciatis cupiditate laborum numquam omnis?</p>
-    <img src="<? echo get_template_directory_uri(); ?><?php echo $maleProfile ?>" alt="headquarters department">
+    <img src="<? echo get_template_directory_uri(); ?><?php echo $logo ?>" alt="profile image">
 </div>
 
 <?php include('templates/partials/footer.php') ?>
